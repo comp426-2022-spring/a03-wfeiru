b9867307878a301c4072ed7931f2aa72cbddbb63
Ruth Wu <rwu02@email.unc.edu>
2022-03-17 21:47:56 -0400

    complete

--- server.js
+++ server.js
@@ -1,22 +1,53 @@
-// Require Express.js
-const express = require('express')
-const app = express()
+import { createRequire } from 'module';
+import { coinFlip, coinFlips, countFlips, flipACoin } from './modules/coin.mjs';
 
-// Start an app server
-const server = app.listen(HTTP_PORT, () => {
-    console.log('App listening on port %PORT%'.replace('%PORT%',HTTP_PORT))
+// require Express.js
+const require = createRequire(import.meta.url);
+const express = require('express');
+const app = express();
+
+// start an app server
+const args = require('minimist')(process.argv.slice(2));
+args['port'];
+const port = args.port||process.env.PORT||3000;
+const server = app.listen(port, () => {
+    console.log('App listening on port %PORT%'.replace('%PORT%',port));
 });
 
-// Default response for any other request
-app.use(function(req, res){
-    res.status(404).send('404 NOT FOUND')
+// endpoint /app/flip/
+app.get('/app/flip/', (req, res) => {
+    const flip = {"flip": coinFlip()};
+    res.json(flip);
 });
 
+// endpoint /app/flips/:number
+app.get('/app/flips/:number', (req, res) => {
+    let flips = coinFlips(req.params.number);
+    let count = countFlips(flips);
+    res.json({"raw": flips, "summary": count});
+});
+
+// endpoint /app/flip/call/heads
+app.get('/app/flip/call/heads', (req, res) => {
+    const record = flipACoin('heads');
+    res.json(record);
+});
+
+// endpoint /app/flip/call/tails
+app.get('/app/flip/call/tails', (req, res) => {
+    const record = flipACoin('tails');
+    res.json(record);
+});
+
+// check endpoint
 app.get('/app/', (req, res) => {
-    // Respond with status 200
-        res.statusCode = 200;
-    // Respond with status message "OK"
-        res.statusMessage = 'OK';
-        res.writeHead( res.statusCode, { 'Content-Type' : 'text/plain' });
-        res.end(res.statusCode+ ' ' +res.statusMessage)
-    });
+    res.statusCode = 200;
+    res.statusMessage = 'OK';
+    res.writeHead(res.statusCode, { 'Content-Type' : 'text/plain' });
+    res.end(res.statusCode+ ' ' +res.statusMessage);
+});
+
+// default response for any other request
+app.use(function(req, res){
+    res.status(404).send('404 NOT FOUND');
+});
